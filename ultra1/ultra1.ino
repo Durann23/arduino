c437e42e5e7cfa523b5c0ffee0df85d0dc0ed741
Durann23 <139575570+Durann23@users.noreply.github.com>
2023-11-15 10:45:13 -0600

    Add files via upload

---
+++ ultra1/ultra1.ino
@@ -0,0 +1,28 @@
+int TRIG[]={1,8,9,10,11,12,13}, ECHO[]={1,2,3,4,5,6,7},dist;
+int dura, sensores=1;
+String cero="0";
+void setup()
+{
+  for(int i=1;i<=sensores; i++){
+    pinMode(TRIG[i], OUTPUT);
+    pinMode(ECHO[i], INPUT);
+    Serial.begin(9600);
+  }
+}
+void loop()
+{
+  for(int i=1;i<=sensores; i++){
+    digitalWrite(TRIG[i], HIGH);
+    delayMicroseconds(1);
+    digitalWrite(TRIG[i], LOW);
+    delayMicroseconds(1); 
+    if(i>9){
+    cero="";
+    }
+    dura=pulseIn(ECHO[i], HIGH);
+    dist=dura/58.2;
+      Serial.print("US"+cero+String(i)+":");
+    Serial.print(dist);
+    Serial.println("cm");
+  }
+}
